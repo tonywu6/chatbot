835c8803e2c93bba7035b24b19388be610c9965a
Tony Wu <tonywu6@protonmail.com>
2023-04-07 13:23:43 +0800

    improve logging

--- cli.py
+++ cli.py
@@ -3,40 +3,47 @@ from contextlib import suppress
 
 import click
 import watchfiles
+from loguru import logger
 
 from dougbot3.bot import create_bot
 from dougbot3.settings import AppSecrets
+from dougbot3.utils.logging import configure_logging
 
 SECRETS = AppSecrets()
 
 
 @click.group()
-def cli():
-    pass
-
-
-async def _main():
-    async with await create_bot() as bot:
-        await bot.start(SECRETS.get_bot_token())
-
-
-def main():
-    with suppress(KeyboardInterrupt):
-        asyncio.run(_main())
+@click.option("--debug", is_flag=True, default=False)
+@click.option("--log-file", type=click.File("a+"), default=None)
+def cli(debug: bool = False, log_file: str | None = None):
+    configure_logging(log_file, level="DEBUG" if debug else "INFO")
 
 
 @cli.command()
 @click.option("--autoreload", is_flag=True, default=False, help="Enable auto-reload.")
-def start(autoreload: bool = False):
+def run(autoreload: bool = False):
     if autoreload:
-        watchfiles.run_process(".", target=main)
-    else:
-        main()
+
+        def on_change(*args, **kwargs):
+            logger.warning("File changes detected. Restarting...")
+
+        watchfiles.run_process(".", target=f"python {__file__} run", callback=on_change)
+
+        return
+
+    async def main():
+        bot = await create_bot()
+        async with bot:
+            await bot.start(SECRETS.get_bot_token())
+
+    with suppress(KeyboardInterrupt):
+        asyncio.run(main())
 
 
 @cli.command()
 async def sync_commands():
-    async with await create_bot() as bot:
+    bot = await create_bot()
+    async with bot:
         await bot.login(SECRETS.get_bot_token())
         await bot.tree.sync()
 
--- dougbot3/utils/logging.py
+++ dougbot3/utils/logging.py
@@ -1,13 +1,15 @@
 import logging
+import sys
+from pathlib import Path
 
-from loguru import logger
+import loguru
 
 
 class InterceptHandler(logging.Handler):
     def emit(self, record):
         # Get corresponding Loguru level if it exists
         try:
-            level = logger.level(record.levelname).name
+            level = loguru.logger.level(record.levelname).name
         except ValueError:
             level = record.levelno
 
@@ -17,10 +19,42 @@ class InterceptHandler(logging.Handler):
             frame = frame.f_back
             depth += 1
 
-        logger.opt(depth=depth, exception=record.exc_info).log(
-            level, record.getMessage()
+        loguru.logger.opt(depth=depth, exception=record.exc_info).log(
+            level,
+            record.getMessage(),
         )
 
 
-def configure_logging():
+def configure_logging(
+    log_file: str | Path | None = None,
+    level: int | str = logging.INFO,
+):
+    def formatter(record: "loguru.Record") -> str:
+        prefix = (
+            "<green>{time:YYYY-MM-DD HH:mm:ss.SSS ZZ}</green>"
+            " <bold><level>{level: <8}</level></bold>"
+        )
+        suffix = "<cyan>[{name}:{function}:{line}]</cyan>"
+        if record["level"].no < logging.WARNING:
+            return f"{prefix} {{message}} {suffix}\n"
+        return f"{prefix} <level>{{message}}</level> {suffix}\n"
+
+    loguru.logger.configure(
+        handlers=[
+            {
+                "sink": log_file or sys.stderr,
+                "level": level,
+                "format": formatter,
+            },
+        ],
+        levels=[
+            {"name": "DEBUG", "color": "<magenta>"},
+            {"name": "DEBUG", "color": "<magenta>"},
+            {"name": "INFO", "color": "<blue>"},
+            {"name": "SUCCESS", "color": "<bold><green>"},
+            {"name": "WARNING", "color": "<yellow>"},
+            {"name": "ERROR", "color": "<red>"},
+            {"name": "CRITICAL", "color": "<bold><red>"},
+        ],
+    )
     logging.basicConfig(handlers=[InterceptHandler()], level=0)
